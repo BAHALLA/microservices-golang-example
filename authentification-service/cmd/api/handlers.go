bd0dddfa62a87950cafa49e913781e95a4a97499
Taoufiq BAHALLA <taoufiq.bahalla@hotmail.com>
2023-07-14 17:16:37 +0200

    logging and mailhog

--- authentification-service/cmd/api/handlers.go
+++ authentification-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -33,6 +35,13 @@ func (app *Config) Authenticate(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	err = app.logRequest("authentication", fmt.Sprintf("%s is logged", user.Email))
+
+	if err != nil {
+		app.errorJson(res, err)
+		return
+	}
+
 	payload := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged as user %s", requestPayload.Email),
@@ -40,4 +49,32 @@ func (app *Config) Authenticate(res http.ResponseWriter, req *http.Request) {
 	}
 	app.writeJson(res, http.StatusAccepted, payload)
 
+}
+
+func (app *Config) logRequest(name, data string) error {
+
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	request, err := http.NewRequest("POST", "http://logger-service:8084/log", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
